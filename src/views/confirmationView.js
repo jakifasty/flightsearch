d112898de054b5fd9afe52de659bdb7208c88b86
Ladydiana <diana.culincu@gmail.com>
2022-03-30 01:20:32 +0200

    WIP: confirmation view

--- src/views/confirmationView.js
+++ src/views/confirmationView.js
@@ -4,19 +4,29 @@ function confirmationView(props) {
 		window.print();
 	}
 
+	function generateID (){
+        return Math.random().toString(36).slice(2);
+  };
+
+	function handleCancelACB(event) {
+        window.location.hash = "#homepage";
+  }
+
 	return (
-        <div className="confirmation">
-			<div className="div-table">
-					<div className="div-row confDate"> {props.flightData.date} </div>
-					<div className="div-row confRoute"> {props.flightData.route} </div>
-					<div className="div-row "> Confirmation number: <span class="confNumber">{props.flightData.confirmationNumber} </span> </div>
-					<div className="div-row confText"> Mobile check-in is not available for this route. </div>
-	
-					<div className="div-row">
-						<div className="div-row confButton"> <button onclick={printPageACB}>Print This Page</button> </div>
-						<div className="div-row confButton"><button>Email confirmation</button></div>			
-					</div>   
-			</div>
+		<div className="container">
+				<div>
+					<h4 className="header4"> Thank you for your reservation. Confirmation number: <b>{generateID()}</b>. <br/>
+					We're dedicated to giving you the best experience possible. If you have any questions, feel free to get in touch. </h4>
+					</div>
+
+					<div className="row">
+						<div className="input-group">
+								<div className="div-row confButton">
+									<button className="inpt col-half btnBk" onClick={handleCancelACB}>Go back to homepage</button>
+								</div>
+						</div>
+					</div>
+
 		</div>
     );
 
--- views/confirmationView.js
+++ views/confirmationView.js
@@ -11,11 +11,11 @@ function confirmationView(props) {
 					<div className="div-row confRoute"> {props.flightData.route} </div>
 					<div className="div-row confNumber"> {props.flightData.confirmationNumber} </div>
 					<div className="div-row confText"> Mobile check-in is not available for this route.</div>
-	
+
 					<div className="div-row>
 						<div className="div-row confButton"> <button onclick={printPageACB}>Print This Page</button> </div>
-						<div className="div-row confButton"><button>Email confirmation</button></div>			
-					</div>   
+						<div className="div-row confButton"><button>Email confirmation</button></div>
+					</div>
 			</div>
 		</div>
     );
