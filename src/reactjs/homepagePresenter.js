a9aff9699c060081fdfc4e8d575e7d9c89b590d9
Marco Godow <godow@kth.se>
2022-04-04 23:18:27 +0200

    Basics of automplete airport search with scripts for getting info

--- src/reactjs/homepagePresenter.js
+++ src/reactjs/homepagePresenter.js
@@ -1,5 +1,6 @@
 import React from "react"
 import HomepageView from "../views/homepageView";
+import { fetchAirports } from "../util";
 //import sendMail from "../testFIle";
 export default
 function Homepage(props){
@@ -7,8 +8,24 @@ function Homepage(props){
     const [, setAdults]=React.useState(null);
     const [, setYouths]=React.useState(null);
     const [, setTripType]=React.useState(null);
+    const [promise, setPromise]=React.useState();
+    const [data, setData]= React.useState([]);
+    const [error, setError]= React.useState([]);
+    const [testAirport, setAirport]= React.useState([]);
+    
+    function searchAirportsACB(searchText) {
+    
+        if(searchText.length >1){
+            var match = data[searchText.substring(0,2).toLocaleUpperCase()]
+            console.log(match)
+            if(searchText.length >2 && match != undefined){
 
-
+                match = match[searchText.substring(0,3).toLocaleUpperCase()]
+            }
+            setAirport(match)
+            console.log(testAirport)
+        } 
+    }
 
     function observerACB(){
         setFrom(props.model.fromAirport);
@@ -16,8 +33,26 @@ function Homepage(props){
         setYouths(props.model.amountOfYouths);
         setTripType(props.model.tripType);
     }
+    function newAirportSearchACB(searchText){
+        function firstFilterCB (airport,search){
+            return (airport[search] != undefined)
+        }
 
+        if(searchText.length >1){
+            var search = searchText.substring(0,2).toLocaleUpperCase()
+            var airports = data.filter(airport => firstFilterCB(airport,search))
+            if(searchText.length>2 && airports != undefined){
+                airports = airports[0][search].filter(airport => firstFilterCB(airport,searchText.substring(0,3).toLocaleUpperCase()))
+                search = searchText.substring(0,3).toLocaleUpperCase()
+            }
+            setAirport(airports[0])
+            //setAirport(Object.entries(airports)[0][1])
+        }
+    }
     function wasCreatedACB(){
+        //setPromise(fetchAirports)
+        var airports = require('../data/airports.json')
+        setData(airports)
         observerACB();
         props.model.addObserver(observerACB);
         return function isTakenDownACB(){         
@@ -76,5 +111,7 @@ function Homepage(props){
         tripType={props.model.tripType}
         dontKnowWhyThisWorkButItDoes={4}
         validRequest={isReadyForSearchACB}
+        searchForAirport= {newAirportSearchACB}
+        airportResults = {testAirport}
         />;
 }
--- src/views/homepageView.js
+++ src/views/homepageView.js
@@ -14,6 +14,39 @@ function HomepageView(props){
     }
     function sendMail(){
     }
+    function toTextInputACB(event){
+        props.searchForAirport(event.target.value)
+    }
+
+    function getListACB(e){
+        var k = Object.keys(e)[0]
+
+        if(k== undefined){
+            console.log("UnDEFINDEDFS")
+            return <datalist id="listID"></datalist>
+        }
+
+        e = e[k]
+
+        if (k.length == 2){
+            console.log("OOOOOO")
+           
+          return <datalist className="textOverflow" id="listID">
+              { e.map(function (airport){
+                var key = Object.keys(airport)
+                airport = airport[key]
+                 return <option className="textOverflow">{airport.airportName + "("+key+")"}</option>
+             }
+             )}
+          </datalist> 
+        }
+        
+        console.log("Returng last")
+
+        return <datalist className="textOverflow" id="listID" >
+            <option className="textOverflow"> {e.airportName + "("+k+")"} </option>
+        </datalist>
+    }
 
     var date = new Date()
     var today = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()
@@ -46,8 +79,8 @@ function HomepageView(props){
                     </div>
                 </div>
                 <div className="outsideBox">
-                    <input className="center" type="text" name="From" placeholder="From" onChange={fromTextInputACB}></input>
-                    <input className={props.tripType ==="One"? "hidden": "center"} type="text" name="Destination" placeholder="Destination"></input>
+                    <input className="center" type="search" list="listID" name="From" placeholder="From" onChange={fromTextInputACB}></input>
+                    <input className={props.tripType ==="One"? "hidden": "center"} type="text"  list="Airports" name="Destination" placeholder="Destination" onChange={toTextInputACB}></input>
                     <input className="center" placeholder="test" type="date" name="trip-start"
                             min={today} max={twoYearsAfterToday}>
                     </input>
@@ -57,13 +90,30 @@ function HomepageView(props){
                 </div>
                 <div className="outsideBox"></div>
                 <button disabled={false/*!props.validRequest*/} onClick={sendMail}>Search</button>
+                <input type="search" list="listID" placeholder="Pick a destination..." onChange={toTextInputACB} onSelect={console.log("Selected")}></input>
+                {getListACB(props.airportResults)}
             </div>
             
     );
 
 }
 
+
+
     /*
+    
+<input type="search" list="languages" placeholder="Pick a programming language..">
+
+<datalist id="languages">
+  <option value="PHP" />
+  <option value="C++" />
+  <option value="Java" />
+  <option value="Ruby" />
+  <option value="Python" />
+  <option value="Go" />
+  <option value="Perl" />
+  <option value="Erlang" />
+</datalist>
      
     */
 export default HomepageView;
