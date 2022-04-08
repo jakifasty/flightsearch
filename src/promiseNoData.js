cf90ddaff1cce134d4c8cf6e887affb239eb05ac
Ladydiana <diana.culincu@gmail.com>
2022-04-08 22:09:06 +0200

    Added plane spinner.

--- src/promiseNoData.js
+++ src/promiseNoData.js
@@ -1,5 +1,5 @@
 function promiseNoData(props){
-  let loading_gif = "https://i.stack.imgur.com/kOnzy.gif";
+  let loading_gif = "https://traveloptionsng.com/wp-content/uploads/2019/09/gogo-loader-plane-transparent.gif";
   let lama = "https://www.icegif.com/wp-content/uploads/icegif-30.gif";
   function render (promise, data, error){
     if(!promise){
