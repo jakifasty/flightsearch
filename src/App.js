05adb595f288a9e0bf0c5e79e46fe8c8449bc25e
javierre <javierre@kth.se>
2022-04-14 18:59:33 +0200

    modification f App.js to include details and future sidebarView

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Homepage from "./reactjs/homepagePresenter.js"
 import Globe from "./reactjs/globePresenter.js";
+//import Details from "./reactjs/detailsPresenter.js";
+//import Sidebar from "./reactjs/sidebarPresenter.js";
+
 const Show=require("./show.js").default;
 require("./navigation.js")
+
 function App(props) {
   return (
     <div className="flexParent">
-      <div className="sidebar"> <Globe/> </div>
+      <div className="sidebar"> 
+        <Globe/>
+        {/*<Show hash="#sidebar"><Sidebar model={props.model}/></Show>*/}
+      </div>
       <div className="mainContent">
       <Show hash="#homepage"><Homepage model={props.model}/></Show>
       {/*<Show hash="#details"><Details model={props.model}/></Show>*/}
