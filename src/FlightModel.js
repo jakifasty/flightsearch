330c8346c9b6ec45a76290162c3991f3af5f2566
Patrik <pateriks@kth.se>
2022-04-15 13:43:21 +0200

    Several merge bug fixes and beatified

--- src/FlightModel.js
+++ src/FlightModel.js
@@ -1,148 +1,209 @@
 import resolvePromise from "./resolvePromise.js";
+import {
+  createPassenger,
+  createPassengers
+} from "./utils.js"
 //import {getFlightDetails} from "./flightSearches"
 /* This is an example of a JavaScript class.
    The Model keeps only abstract data and has no notions of graohics or interaction
 */
 
-function isValid(id){
+function isValid(id) {
   return (typeof(id) == "number")
 }
 
-class FlightModel{
-    constructor(){
-        this.oneWay = "One";
-        this.roundTrip = "Round";
-        this.fromAirport = "LHR";
-        this.destAirport = "JFK";
-        this.observers = [];
-        this.searchResultsPromiseState = {};
-        this.searchParams = {query: "", type: ""};
-        this.amountOfAdults = 1
-        this.amountOfYouths = 0
-        this.tripType = "One"
-        this.data = {slices:
-                      [
-                        {
-                          origin: this.fromAirport,
-                          destination: this.destAirport,
-                          departure_date: '2022-04-24',
-                        },
-                      ],
-                      passengers: [
-                        {
-                          type: 'adult',
-                        },
-                        {
-                          age: 14,
-                        },
-                      ],
-                    cabin_class: 'economy',
-                  };
-        this.roundtripData = {slices:
-                  [
-                    {
-                      origin: 'LHR',
-                      destination: 'JFK',
-                      departure_date: "2022-10-10T17:22:07.481Z"
-                    },
-                    {
-                      origin: 'JFK',
-                      destination: 'LHR',
-                      departure_date: "2022-10-18T17:22:07.481Z"
-                    },
-                  ],
-                  passengers: [{ "type": "adult" }],
-                  cabin_class: null
-                }
+class FlightModel {
+  constructor() {
+    //Not sure why these were added
+    this.oneWay = "One";
+    this.roundTrip = "Round";
+    this.deptDate = "2022-04-24"
+    this.passengers = [];
+    this.cabin_class = "economy";
+    this.fromAirport = "";
+    this.toAirport = "";
+
+    this.amountAdults = 1
+    this.amountYouths = 0
+
+    this.tripType = "One"
+
+    this.searchResultsPromiseState = {};
+    this.observers = [];
+    this.searchParams = {
+      query: "",
+      type: ""
+    };
+
+
+    var date = new Date()
+    this.fromDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+    this.returnDate = (parseInt(this.fromDate.split("-")[0]) + 2) + this.fromDate.slice(this.fromDate.indexOf("-"), this.fromDate.length)
+
+    this.data = {
+      slices: [{
+        origin: 'LHR',
+        destination: 'JFK',
+        departure_date: this.deptDate
+      }, ],
+      passengers: this.passengers,
+      cabin_class: this.cabin_class,
+    };
+    this.roundtripData = {
+      slices: [{
+          origin: 'LHR',
+          destination: 'JFK',
+          departure_date: this.deptDate
+        },
+        {
+          origin: 'JFK',
+          destination: 'LHR',
+          departure_date: this.returnDate
+        },
+      ],
+      passengers: this.passengers,
+      cabin_class: this.cabin_class,
     }
-
-    /*setCurrentFlight(id){
-      let myModel = this;
-
-      function notifyACB() {
-        myModel.notifyObservers(null);
-      }
-
-      if(!id || id==this.currentDish || !isValid(id)) return;
-
-      resolvePromise(getFlightDetails(id), this.currentFlightPromiseState, notifyACB);
-
-      this.currentFlight = id;
-
-      this.notifyObservers({setCurrentFlight: id})
-    }*/
-
-    setTripType(type){
-        this.tripType = type
-        var payload = {tripType: type}
-        this.notifyObservers(payload)
-    }
-
-    setFromAirport(airport){
-        this.fromAirport = airport
-        var payload = {fromAirport: airport}
-        this.notifyObservers(payload)
-    }
-
-    setToAirport(airport){
-      this.toAirport = airport
-      var payload = {toAirport: airport}
-      this.notifyObservers(payload)
   }
 
-    setAmountAdults(nr){
-        this.amountOfAdults = nr
-        var payload = {amountAdults: nr}
-        this.notifyObservers(payload)
-
-    }
-    setAmountYouths(nr){
-        this.amountOfYouths = nr
-        var payload = {amountYouths: nr}
-        this.notifyObservers(payload)
-    }
 
 
-     setSearchQuery(q){
-       this.searchParams.query = q;
-       this.notifyObservers();
-     }
-     setSearchType(t){
-       this.searchParams.type = t;
-       this.notifyObservers();
-     }
-     doSearch(params){
-       const theModel = this;
-       function notifyACB() {theModel.notifyObservers(null);};
-       if(params){
-         resolvePromise(null, this.searchResultsPromiseState, notifyACB);
-       }
-       else{
-         resolvePromise(null, this.searchResultsPromiseState, notifyACB);
-       }
-     }
-     addObserver(callback){
-       this.observers.push(callback);
-     }
-     removeObserver(callback){
-        this.observers = this.observers.filter(function(x){
-            if(x === callback){
-                return false
-            }
-            return true;
-            }
-        )
+  setFromDate(date) {
+    this.fromDate = date
+    var payload = {
+      fromDate: date
     }
-     notifyObservers(payload){
-        function invokeObserverCB(obs){
-          try{
-            obs(payload);
-          }catch (err){
-            console.log(err);
-          }
-        }
-        this.observers.forEach(invokeObserverCB);
+    this.notifyObservers(payload)
+  }
+
+  setReturnDate(date) {
+    this.returnDate = date
+    var payload = {
+      returnDate: date
+    }
+    this.notifyObservers(payload)
+  }
+
+
+  setTripType(type) {
+    this.tripType = type
+    var payload = {
+      tripType: type
+    }
+    this.notifyObservers(payload)
+  }
+
+  setFromAirport(airport) {
+    this.fromAirport = airport
+    var payload = {
+      fromAirport: airport
+    }
+    this.notifyObservers(payload)
+  }
+
+  setToAirport(airport) {
+    this.toAirport = airport
+    var payload = {
+      toAirport: airport
+    }
+    this.notifyObservers(payload)
+  }
+
+  setAmountAdults(nr) {
+    this.amountAdults = nr
+    var payload = {
+      amountAdults: nr
+    }
+    this.notifyObservers(payload)
+
+  }
+  setAmountYouths(nr) {
+    this.amountYouths = nr
+    var payload = {
+      amountYouths: nr
+    }
+    this.notifyObservers(payload)
+  }
+
+
+  setSearchQuery(q) {
+    this.searchParams.query = q;
+    this.notifyObservers();
+  }
+  setSearchType(t) {
+    this.searchParams.type = t;
+    this.notifyObservers();
+  }
+  doSearch(params) {
+    const theModel = this;
+
+    function notifyACB() {
+      theModel.notifyObservers(null);
+    };
+    if (params) {
+      resolvePromise(null, this.searchResultsPromiseState, notifyACB);
+    } else {
+      resolvePromise(null, this.searchResultsPromiseState, notifyACB);
+    }
+  }
+  addPassenger(passenger) {
+    this.passengers = this.passengers.push(passenger) //TODO: check passenger structure
+  }
+
+  addPassengers(passengers) {
+    this.passengers = this.passengers.concat(passengers); //TODO: check passenger structure
+  }
+  makePassengers() {
+    let childPassengers = createPassengers(this.amountYouths, "child");
+    let adultPassengers = createPassengers(this.amountAdults, "adult");
+    this.addPassengers(childPassengers);
+    this.addPassengers(adultPassengers);
+    this.data.passengers = this.passengers;
+  }
+
+  addChildToData(amount) {
+    let childPassengers = createPassengers((amount ? amount : 1), "child");
+    this.addPassengers(childPassengers);
+  }
+
+  addAdultToData(amount) {
+    let adultPassengers = createPassengers((amount ? amount : 1), "adult");
+    this.addPassengers(adultPassengers);
+  }
+
+  makeData() {
+    if (this.amountOfAdults < 1 && this.amountOfYouths < 1) {
+      throw new Error('number of people is zero');
+    }
+    this.makePassengers();
+  }
+
+  clearData() {
+    this.passengers = [];
+    this.data.passengers = this.passengers;
+  }
+
+  addObserver(callback) {
+    this.observers.push(callback);
+  }
+  removeObserver(callback) {
+    this.observers = this.observers.filter(function(x) {
+      if (x === callback) {
+        return false
       }
+      return true;
+    })
+  }
+  notifyObservers(payload) {
+    function invokeObserverCB(obs) {
+      try {
+        obs(payload);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    this.observers.forEach(invokeObserverCB);
+  }
 
 }
 
--- src/reactjs/homepagePresenter.js
+++ src/reactjs/homepagePresenter.js
@@ -3,191 +3,251 @@ import HomepageFormView from "../views/homepageFormView";
 import HomepageResultsView from "../views/homepageResultsView";
 import resolvePromise from "../resolvePromise"
 import promiseNoData from "../promiseNoData"
-import  {getAirportsInCity, getOffers} from "../flightSearches.js";
+import {
+  getAirportsInCity,
+  getOffers
+} from "../flightSearches.js";
 //import sendMail from "../testFIle";
 
 export default
-    function Homepage(props) {
-    const [, setFrom] = React.useState(null);
-    const [, setTo] = React.useState(null);
-    const [, setFromDate] = React.useState(null);
-    const [, setReturnDate] = React.useState(null);
-    const [, setAdults] = React.useState(null);
-    const [, setYouths] = React.useState(null);
-    const [, setTripType] = React.useState(null);
-    const [choosenAirport, setAirport] = React.useState([]);
-    const [airportsPromiseState]=React.useState({});
-    const [flightPromiseState]=React.useState({});
-    const [, reRender]=React.useState();
 
-    const data = require('../data/airports.json')
+function Homepage(props) {
+  const [, setFrom] = React.useState(null);
+  const [, setTo] = React.useState(null);
+  const [, setFromDate] = React.useState(null);
+  const [, setReturnDate] = React.useState(null);
+  const [, setAdults] = React.useState(null);
+  const [, setYouths] = React.useState(null);
+  const [, setTripType] = React.useState(null);
+  const [choosenAirport, setAirport] = React.useState([]);
+  const [airportsPromiseState] = React.useState({});
+  const [flightPromiseState] = React.useState({});
+  const [, reRender] = React.useState();
+
+  const data = require('../data/airports.json')
 
 
-    //TODO add return airports functionality
-    function resolveAirports(promise){
-      resolvePromise(promise, airportsPromiseState,
-        function promiseStateChangedACB(){reRender(new Object());})
+  //TODO add return airports functionality
+  function resolveAirports(promise) {
+    resolvePromise(promise, airportsPromiseState,
+      function promiseStateChangedACB() {
+        reRender(new Object());
+      })
+  }
+
+  function resolveFlight(promise) {
+    resolvePromise(promise, flightPromiseState,
+      function promiseStateChangedACB() {
+        reRender(new Object());
+      })
+  }
+
+  function observerACB() {
+    setFrom(props.model.fromAirport);
+    setTo(props.model.toAirport)
+    setFromDate(props.model.fromDate)
+    setReturnDate(props.model.returnDate)
+    setAdults(props.model.amountAdults);
+    setYouths(props.model.amountYouths);
+    setTripType(props.model.tripType);
+  }
+
+  function searchAirportACB(searchText) {
+
+    function isValidAirportCB(airport, search) {
+      return (airport[search] != undefined)
     }
 
-    function resolveFlight(promise){
-      resolvePromise(promise, flightPromiseState,
-        function promiseStateChangedACB(){
-          reRender(new Object());})
-    }
-
-    function observerACB(){
-        setFrom(props.model.fromAirport);
-        setTo(props.model.toAirport)
-        setFromDate(props.model.fromDate)
-        setReturnDate(props.model.returnDate)
-        setAdults(props.model.amountAdults);
-        setYouths(props.model.amountYouths);
-        setTripType(props.model.tripType);
-    }
-
-    function searchAirportACB(searchText) {
-
-        function isValidAirportCB(airport, search) {
-            return (airport[search] != undefined)
-        }
-
-        //TODO cleanup/make more efficent
-        if (searchText.length > 1) {
-            var search = searchText.substring(0, 2).toLocaleUpperCase()
-            var airports = data.filter(airport => isValidAirportCB(airport, search))
-            if (searchText.length > 2 && airports != undefined) {
-                airports = airports[0][search].filter(airport => isValidAirportCB(airport, searchText.substring(0, 3).toLocaleUpperCase()))
-                search = searchText.substring(0, 3).toLocaleUpperCase()
-            }
-            setAirport(airports[0])
-        } else {
-            setAirport([])
-        }
-    }
-
-    function wasCreatedACB() {
-        //var airports = require('../data/airports.json')
-        //setData(airports)
-        observerACB();
-        props.model.addObserver(observerACB);
-        return function isTakenDownACB() {
-            props.model.removeObserver(observerACB);
-        }
-    }
-    React.useEffect(wasCreatedACB, []);
-
-    function onFromAirportSelectACB(from) {
-        props.model.setFromAirport(from)
-    }
-
-    function onToAirportSelectACB(to) {
-        props.model.setToAirport(to)
-
-    }
-
-    function onChangeAmountPeopleACB(params) {
-        switch (params) {
-            case 'Adult +':
-                props.model.setAmountAdults(props.model.amountOfAdults + 1)
-                break;
-            case 'Adult -':
-                props.model.setAmountAdults(props.model.amountOfAdults - 1)
-                break;
-            case 'Youth +':
-                props.model.setAmountYouths(props.model.amountOfYouths + 1)
-                break;
-            case 'Youth -':
-                props.model.setAmountYouths(props.model.amountOfYouths - 1)
-                break;
-            default:
-                break;
-        }
+    //TODO cleanup/make more efficent
+    if (searchText.length > 1) {
+      var search = searchText.substring(0, 2).toLocaleUpperCase()
+      var airports = data.filter(airport => isValidAirportCB(airport, search))
+      if (searchText.length > 2 && airports != undefined) {
+        airports = airports[0][search].filter(airport => isValidAirportCB(airport, searchText.substring(0, 3).toLocaleUpperCase()))
+        search = searchText.substring(0, 3).toLocaleUpperCase()
       }
+      setAirport(airports[0])
+    } else {
+      setAirport([])
+    }
+  }
 
-    function compareDates(date1,date2){
-        var date1Split = date1.split("-")
-        var date2Split = date2.split("-")
-        if(parseInt(date1Split[0])>parseInt(date2Split[0])){
-            return false
-        }
-        if(parseInt(date1Split[1])>parseInt(date2Split[1])){
-            return false
+  function wasCreatedACB() {
+    //var airports = require('../data/airports.json')
+    //setData(airports)
+    observerACB();
+    props.model.addObserver(observerACB);
+    return function isTakenDownACB() {
+      props.model.removeObserver(observerACB);
+    }
+  }
+  React.useEffect(wasCreatedACB, []);
 
-        }
-        if(parseInt(date1Split[2])>parseInt(date2Split[2])){
-            return false
-        }
-        return true
+  function onFromAirportSelectACB(from) {
+    props.model.setFromAirport(from)
+  }
+
+  function onToAirportSelectACB(to) {
+    props.model.setToAirport(to)
+
+  }
+
+  function onChangeAmountPeopleACB(params) {
+    switch (params) {
+      case 'Adult +':
+        props.model.setAmountAdults(props.model.amountAdults + 1)
+        break;
+      case 'Adult -':
+        props.model.setAmountAdults(props.model.amountAdults - 1)
+        break;
+      case 'Youth +':
+        props.model.setAmountYouths(props.model.amountYouths + 1)
+        break;
+      case 'Youth -':
+        props.model.setAmountYouths(props.model.amountYouths - 1)
+        break;
+      default:
+        break;
+    }
+  }
+
+  function compareDates(date1, date2) {
+    var date1Split = date1.split("-")
+    var date2Split = date2.split("-")
+    if (parseInt(date1Split[0]) > parseInt(date2Split[0])) {
+      return false
+    }
+    if (parseInt(date1Split[1]) > parseInt(date2Split[1])) {
+      return false
+
+    }
+    if (parseInt(date1Split[2]) > parseInt(date2Split[2])) {
+      return false
+    }
+    return true
+  }
+
+  function onSelectFromDateACB(date) {
+    if (compareDates(date, props.model.returnDate) || props.model.tripType == props.model.oneWay) {
+      props.model.setFromDate(date)
+    }
+  }
+
+  function onSelectReturnDateACB(date) {
+    if (compareDates(props.model.fromDate, date)) {
+      props.model.setReturnDate(date)
+    }
+  }
+
+  function onSelectTripTypeACB(type) {
+    props.model.setTripType(type)
+  }
+
+  //TODO
+  function isReadyForSearchACB() {
+
+    function checkRest() {
+
     }
 
-    function onSelectFromDateACB(date) {
-        if(compareDates(date,props.model.returnDate) || props.model.tripType == props.model.oneWay){
-            props.model.setFromDate(date)
-        }
-    }
-
-    function onSelectReturnDateACB(date) {
-        if(compareDates(props.model.fromDate,date)){
-            props.model.setReturnDate(date)
-        }
-    }
-
-    function onSelectTripTypeACB(type) {
-        props.model.setTripType(type)
-    }
-
-    //TODO
-    function isReadyForSearchACB() {
-
-        function checkRest(){
+    if (props.model.amountAdults + props.model.amountYouths > 0) {
+      if (props.model.tripType === props.model.oneWay) {
+        if (props.model.fromAirport !== '') {
 
         }
+      } else if (props.model.tripType === props.model.roundTrip) {
 
-        if (props.model.amountOfAdults + props.model.amountOfYouths > 0) {
-            if (props.model.tripType === props.model.oneWay) {
-                if (props.model.fromAirport !== '') {
-
-                }
-            } else if (props.model.tripType === props.model.roundTrip) {
-
-            } else {
-                return false;
-            }
-        }
+      } else {
+        return false;
+      }
     }
+  }
 
-    function searchACB(){
-        if(props.model.tripType == props.model.oneWay)
-          resolveFlight(getOffers(props.model.data));
-        else if(props.model.tripType == props.model.roundTrip)
-          resolveFlight(getOffers(props.model.roundtripData));
+  function searchACB() {
+
+
+    if (props.model.tripType == props.model.oneWay)
+      try {
+        props.model.makeData();
+        resolveFlight(getOffers(props.model.data));
+        props.model.clearData()
+      } catch (error) {
+        console.log(error);
+      }
+    else if (props.model.tripType == props.model.roundTrip)
+      try {
+        props.model.makeData();
+        resolveFlight(getOffers(props.model.roundtripData));
+        props.model.clearData()
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
+  function changeFlightOnClickACB(flight) {
+    //props.model.setCurrentFlight(flight.id);
+  }
+
+  return <div > < HomepageFormView
+  onChangeAmountPeople = {
+    onChangeAmountPeopleACB
+  }
+  onFromAirportSelect = {
+    onFromAirportSelectACB
+  }
+  onToAirportSelect = {
+    onToAirportSelectACB
+  }
+  onSelectTripType = {
+    onSelectTripTypeACB
+  }
+  onSelectFromDate = {
+    onSelectFromDateACB
+  }
+  onSelectReturnDate = {
+    onSelectReturnDateACB
+  }
+  isValidRequest = {
+    isReadyForSearchACB
+  }
+  onSearchForAirport = {
+    searchAirportACB
+  }
+  onSearch = {
+    searchACB
+  }
+  fromAirport = {
+    props.model.fromAirport
+  }
+  amountOfPeople = {
+    props.model.amountAdults + props.model.amountYouths
+  }
+  amountAdults = {
+    props.model.amountAdults
+  }
+  amountYouths = {
+    props.model.amountYouths
+  }
+  tripType = {
+    props.model.tripType
+  }
+  airportResults = {
+    choosenAirport
+  }
+  /> {
+    promiseNoData({
+        promise: flightPromiseState.promise,
+        data: flightPromiseState.data,
+        error: flightPromiseState.error
+      }) ||
+      < HomepageResultsView results = {
+        flightPromiseState.data
+      }
+    onChooseFlight = {
+      changeFlightOnClickACB
     }
-
-    function changeFlightOnClickACB(flight){
-        //props.model.setCurrentFlight(flight.id);
-    }
-
-    return <div> < HomepageFormView
-        onChangeAmountPeople={onChangeAmountPeopleACB}
-        onFromAirportSelect={onFromAirportSelectACB}
-        onToAirportSelect={onToAirportSelectACB}
-        onSelectTripType={onSelectTripTypeACB}
-        onSelectFromDate={onSelectFromDateACB}
-        onSelectReturnDate={onSelectReturnDateACB}
-        isValidRequest={isReadyForSearchACB}
-        onSearchForAirport={searchAirportACB}
-        onSearch={searchACB}
-        fromAirport={props.model.fromAirport}
-        amountOfPeople={props.model.amountAdults + props.model.amountYouths}
-        amountOfAdults={props.model.amountAdults}
-        amountOfYouths={props.model.amountYouths}
-        tripType={props.model.tripType}
-        airportResults={choosenAirport}
     />
-    {
-        promiseNoData({promise: flightPromiseState.promise, data: flightPromiseState.data, error: flightPromiseState.error})
-        || <HomepageResultsView results={flightPromiseState.data} onChooseFlight={changeFlightOnClickACB}/>
-    };
-    </div>
+  }; <
+  /div>
 
 }
