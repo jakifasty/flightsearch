513f3f01a82a1bbf8d2508e3323304dcdf7f4595
Ladydiana <diana.culincu@gmail.com>
2022-05-02 21:50:04 +0200

    Replaced for with htmlFor

--- src/views/bookingView.js
+++ src/views/bookingView.js
@@ -10,9 +10,9 @@ function bookingView(props) {
 	          <div className="div-row confDate"> Arrival date: {props.flightData.returnDate} </div>
 						<div className="div-row confRoute"> From: {props.flightData.fromAirport} </div>
 	          <div className="div-row confRoute"> To: {props.flightData.toAirport} </div>
-						<label for="name" required>Name:</label> <input type="text" id="name" name="user_name"/>
-						<label for="surname" required>Surname:</label> <input type="text" id="surname" name="user_surname"/>
-						<label for="mail" required>E-mail:</label> <input type="email" id="mail" name="user_email"/>
+						<label htmlFor="name" required>Name:</label> <input type="text" id="name" name="user_name"/>
+						<label htmlFor="surname" required>Surname:</label> <input type="text" id="surname" name="user_surname"/>
+						<label htmlFor="mail" required>E-mail:</label> <input type="email" id="mail" name="user_email"/>
 						<div className="div-row">
 								<div className="div-row confButton"><button>Finish</button></div>
 						</div>
