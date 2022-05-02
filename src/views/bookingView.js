fcdb44e11d787b0e3bd45752608e6c7c3514e295
Ladydiana <diana.culincu@gmail.com>
2022-05-02 22:34:12 +0200

    Added divs

--- src/views/bookingView.js
+++ src/views/bookingView.js
@@ -1,6 +1,7 @@
 function bookingView(props) {
 
 	console.log(props.flightData);
+	console.log(props.model);
 
 
 	return (
@@ -10,9 +11,9 @@ function bookingView(props) {
 	          <div className="div-row confDate"> Arrival date: {props.flightData.returnDate} </div>
 						<div className="div-row confRoute"> From: {props.flightData.fromAirport} </div>
 	          <div className="div-row confRoute"> To: {props.flightData.toAirport} </div>
-						<label htmlFor="name" required>Name:</label> <input type="text" id="name" name="user_name"/>
-						<label htmlFor="surname" required>Surname:</label> <input type="text" id="surname" name="user_surname"/>
-						<label htmlFor="mail" required>E-mail:</label> <input type="email" id="mail" name="user_email"/>
+						<div> <label htmlFor="name" required>Name:</label> <input type="text" id="name" name="user_name"/> </div>
+						<div> <label htmlFor="surname" required>Surname:</label> <input type="text" id="surname" name="user_surname"/> </div>
+						<div> <label htmlFor="mail" required>E-mail:</label> <input type="email" id="mail" name="user_email"/> </div>
 						<div className="div-row">
 								<div className="div-row confButton"><button>Finish</button></div>
 						</div>
