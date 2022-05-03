0a1a2fc7e3ed5ef057f5419d03355bc1a6907333
Ladydiana <diana.culincu@gmail.com>
2022-05-04 00:50:36 +0200

    Change of layout.

--- src/views/bookingView.js
+++ src/views/bookingView.js
@@ -10,75 +10,103 @@ function bookingView(props) {
 
 
 	return (
-      <div >
-				<div >
+      <div class="container">
+			<div>
 						<h4 className="header4"> Flight Information </h4>
 						<div className="div-row confDate"> Departure date: {props.flightData.deptDate} </div>
 	          <div className="div-row confDate"> Arrival date: {props.flightData.returnDate} </div>
 						<div className="div-row confRoute"> From: {props.flightData.fromAirport} </div>
 	          <div className="div-row confRoute"> To: {props.flightData.toAirport} </div>
-						<h4 className="header4"> Personal Information </h4>
-						<div> <input type="text" id="name" name="user_name" placeholder="Full Name"/> </div>
-						<div> <input type="email" id="mail" name="user_email" placeholder="Email Adress"/> </div>
-						<div>
-							<h4 className="header4"> Date of Birth </h4>
-							<input type="text" id="DD" name="DD" placeholder="DD"/>
-							<input type="text" id="DD" name="MM" placeholder="MM"/>
-							<input type="text" id="YYYY" name="YYYY" placeholder="YYYY"/>
-						</div>
-						<div>
-							<h4 className="header4"> Gender </h4>
-							<input type="radio" className="rad" id="gender-male" name="gender" value="male"/> <label htmlFor="gender-male">Male</label>
-							<input type="radio" className="rad" id="gender-female" name="gender" value="female"/> <label htmlFor="gender-female">Female</label>
-						</div>
-
-						<div>
-							<h4 className="header4"> Payment details </h4>
-							<input type="radio" className="rad" id="payment-method-card" name="payment-method" value="card" defaultChecked/> <label htmlFor="payment-method-card">Credit Card</label>
-							<input type="radio" className="rad" id="payment-method-paypal" name="payment-method" value="paypal" disabled={true}/> <label htmlFor="payment-method-paypal">Paypal</label>
-							<div>
-								<input type="text" id="card-number" name="card-number" placeholder="Card Number"/>
-								<input type="text" id="card-cvc" name="card-cvc" placeholder="Card CVC"/>
-								<select name="card-month" id="card-month" className="dropbtn2">
-								  <option value="jan">01 Jan</option>
-								  <option value="feb">02 Feb</option>
-								  <option value="mar">03 Mar</option>
-								  <option value="apr">04 Apr</option>
-									<option value="may">05 May</option>
-									<option value="jun">06 Jun</option>
-									<option value="jul">07 Jul</option>
-									<option value="aug">08 Aug</option>
-									<option value="sept">09 Sept</option>
-									<option value="oct">10 Oct</option>
-									<option value="nov">11 Nov</option>
-									<option value="dec">12 Dec</option>
-								</select>
-
-								<select name="card-year" id="card-year" className="dropbtn2">
-								  <option value="2022">2022</option>
-								  <option value="2023">2023</option>
-								  <option value="2024">2024</option>
-								  <option value="2025">2025</option>
-									<option value="2026">2026</option>
-									<option value="2027">2027</option>
-									<option value="2028">2028</option>
-									<option value="2029">2029</option>
-									<option value="2030">2030</option>
-								</select>
-							</div>
-						</div>
-
-						<div>
-							<h4 className="header4"> Terms and Conditions </h4>
-							<input type="checkbox" id="terms" className="check" name="terms" value="terms" defaultChecked disabled={true}/> <label htmlFor="terms">By clicking Finish, I accept the terms and conditions for signing up to this service, and hereby confirm I have read the privacy policy.</label>
-						</div>
-
-						<div className="div-row">
-								<div className="div-row confButton"><button>Finish</button></div>
-								<div className="div-row confButton"><button onClick={handleCancelACB}>Cancel</button></div>
-						</div>
 			</div>
-		</div>
+
+
+			  <form>
+			    <div class="row">
+			      <h4>Account</h4>
+			      <div class="input-group input-group-icon">
+			        <input type="text" placeholder="Full Name"/>
+			        <div class="input-icon"><i class="fa fa-user"></i></div>
+			      </div>
+			      <div class="input-group input-group-icon">
+			        <input type="email" placeholder="Email Adress"/>
+			        <div class="input-icon"><i class="fa fa-envelope"></i></div>
+			      </div>
+			      <div class="input-group input-group-icon">
+			        <input type="password" placeholder="Password"/>
+			        <div class="input-icon"><i class="fa fa-key"></i></div>
+			      </div>
+			    </div>
+			    <div class="row">
+			      <div class="col-half">
+			        <h4>Date of Birth</h4>
+			        <div class="input-group">
+			          <div class="col-third">
+			            <input type="text" placeholder="DD"/>
+			          </div>
+			          <div class="col-third">
+			            <input type="text" placeholder="MM"/>
+			          </div>
+			          <div class="col-third">
+			            <input type="text" placeholder="YYYY"/>
+			          </div>
+			        </div>
+			      </div>
+			      <div class="col-half">
+			        <h4>Gender</h4>
+			        <div class="input-group">
+			          <input id="gender-male" type="radio" name="gender" value="male"/>
+			          <label for="gender-male">Male</label>
+			          <input id="gender-female" type="radio" name="gender" value="female"/>
+			          <label for="gender-female">Female</label>
+			        </div>
+			      </div>
+			    </div>
+			    <div class="row">
+			      <h4>Payment Details</h4>
+			      <div class="input-group">
+			        <input id="payment-method-card" type="radio" name="payment-method" value="card" checked="true"/>
+			        <label for="payment-method-card"><span><i class="fa fa-cc-visa"></i>Credit Card</span></label>
+			        <input id="payment-method-paypal" type="radio" name="payment-method" value="paypal"/>
+			        <label for="payment-method-paypal"> <span><i class="fa fa-cc-paypal"></i>Paypal</span></label>
+			      </div>
+			      <div class="input-group input-group-icon">
+			        <input type="text" placeholder="Card Number"/>
+			        <div class="input-icon"><i class="fa fa-credit-card"></i></div>
+			      </div>
+			      <div class="col-half">
+			        <div class="input-group input-group-icon">
+			          <input type="text" placeholder="Card CVC"/>
+			          <div class="input-icon"><i class="fa fa-user"></i></div>
+			        </div>
+			      </div>
+			      <div class="col-half">
+			        <div class="input-group">
+			          <select>
+			            <option>01 Jan</option>
+			            <option>02 Jan</option>
+			          </select>
+			          <select>
+			            <option>2015</option>
+			            <option>2016</option>
+			          </select>
+			        </div>
+			      </div>
+			    </div>
+			    <div class="row">
+			      <h4>Terms and Conditions</h4>
+			      <div class="input-group">
+			        <input id="terms" type="checkbox"/>
+			        <label for="terms">I accept the terms and conditions for signing up to this service, and hereby confirm I have read the privacy policy.</label>
+			      </div>
+			    </div>
+			  </form>
+			</div>
+
+
+
+
+
+
     );
 
 }
