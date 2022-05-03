3cf2368fa77d2c80befe9d00c569f2df5dd68b53
javierre <javierre@kth.se>
2022-05-03 11:10:11 +0200

    modification of details and homepageResults view

--- src/views/detailsView.js
+++ src/views/detailsView.js
@@ -1,5 +1,36 @@
 function DetailsView(props){
+  console.log("here value flighData");
   console.log(props.flightData);
-  return <div> Details about chosen flight {JSON.stringify(props.flightData)}</div>
+
+  function clickAddToCartCB(flight){
+		window.location.hash="#search";
+		return props.onAddToFinalList(flight);
+	}
+
+  function clickRemoveFromCart(flight){
+    return props.searchFlights(flight);
+  }
+
+  function setToReturnkACB() {
+    window.location.hash="#search";
+  }
+
+  return  <div >
+            Details about chosen flight {props.flightData.slices[0].segments[0].operating_carrier.iata_code + props.flightData.slices[0].segments[0].operating_carrier_flight_number}
+            {props.flightData.owner.name}
+            <div className="flightResults">
+              <div>
+                <span><h1>{props.flightData}</h1></span>
+              </div>
+            </div>
+            <tr>
+            {/*JSON.stringify(props.flightData)*/}
+            </tr>
+            <div>
+              <button type="button" onClick={clickAddToCartCB} disabled={props.isFlightInCart}>Add to cart</button>
+              <button onclick={setToReturnkACB}>Return to search</button>
+            </div>
+          </div>
 }
+
 export default DetailsView;
