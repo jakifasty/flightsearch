d01412787935f8834e66cca057a1185062ffa0f8
Ladydiana <diana.culincu@gmail.com>
2022-05-03 23:41:37 +0200

    Styling struggles.

--- src/views/bookingView.js
+++ src/views/bookingView.js
@@ -10,36 +10,36 @@ function bookingView(props) {
 
 
 	return (
-      <div className="book">
-				<div className="div-table">
-						<h4> Flight Information </h4>
+      <div >
+				<div >
+						<h4 className="header4"> Flight Information </h4>
 						<div className="div-row confDate"> Departure date: {props.flightData.deptDate} </div>
 	          <div className="div-row confDate"> Arrival date: {props.flightData.returnDate} </div>
 						<div className="div-row confRoute"> From: {props.flightData.fromAirport} </div>
 	          <div className="div-row confRoute"> To: {props.flightData.toAirport} </div>
-						<h4> Personal Information </h4>
+						<h4 className="header4"> Personal Information </h4>
 						<div> <input type="text" id="name" name="user_name" placeholder="Full Name"/> </div>
 						<div> <input type="email" id="mail" name="user_email" placeholder="Email Adress"/> </div>
 						<div>
-							<h4> Date of Birth </h4>
+							<h4 className="header4"> Date of Birth </h4>
 							<input type="text" id="DD" name="DD" placeholder="DD"/>
 							<input type="text" id="DD" name="MM" placeholder="MM"/>
 							<input type="text" id="YYYY" name="YYYY" placeholder="YYYY"/>
 						</div>
 						<div>
-							<h4> Gender </h4>
+							<h4 className="header4"> Gender </h4>
 							<input type="radio" id="gender-male" name="gender" value="male"/> <label htmlFor="gender-male">Male</label>
 							<input type="radio" id="gender-female" name="gender" value="female"/> <label htmlFor="gender-female">Female</label>
 						</div>
 
 						<div>
-							<h4> Payment details </h4>
+							<h4 className="header4"> Payment details </h4>
 							<input type="radio" id="payment-method-card" name="payment-method" value="card" defaultChecked/> <label htmlFor="payment-method-card">Credit Card</label>
 							<input type="radio" id="payment-method-paypal" name="payment-method" value="paypal" disabled={true}/> <label htmlFor="payment-method-paypal">Paypal</label>
 							<div>
 								<input type="text" id="card-number" name="card-number" placeholder="Card Number"/>
 								<input type="text" id="card-cvc" name="card-cvc" placeholder="Card CVC"/>
-								<select name="card-month" id="card-month">
+								<select name="card-month" id="card-month" className="dropbtn2">
 								  <option value="jan">01 Jan</option>
 								  <option value="feb">02 Feb</option>
 								  <option value="mar">03 Mar</option>
@@ -54,7 +54,7 @@ function bookingView(props) {
 									<option value="dec">12 Dec</option>
 								</select>
 
-								<select name="card-year" id="card-year">
+								<select name="card-year" id="card-year" className="dropbtn2">
 								  <option value="2022">2022</option>
 								  <option value="2023">2023</option>
 								  <option value="2024">2024</option>
@@ -69,7 +69,7 @@ function bookingView(props) {
 						</div>
 
 						<div>
-							<h4> Terms and Conditions </h4>
+							<h4 className="header4"> Terms and Conditions </h4>
 							<input type="checkbox" id="terms" name="terms" value="terms" defaultChecked disabled={true}/> <label htmlFor="terms">By clicking Finish, I accept the terms and conditions for signing up to this service, and hereby confirm I have read the privacy policy.</label>
 						</div>
 
