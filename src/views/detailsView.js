7785286da4e492ea1a98ea7e3aa6f08535b655da
javierre <javierre@kth.se>
2022-05-12 00:02:18 +0200

    modifications in CSS and pharagrafs is detailsView

--- src/views/detailsView.js
+++ src/views/detailsView.js
@@ -2,6 +2,8 @@ function DetailsView(props){
   
   console.log("here value flighData");
   console.log(props.flightData);
+  console.log(props.flightData.data.slices[0].segments.length);
+  let index = props.flightData.data.slices[0].segments.length;
 
   function clickAddToCartCB(flight){
     return props.onAddToFinalList(flight);
@@ -37,32 +39,38 @@ function DetailsView(props){
     valueTravel = "specific ducuments required";
   }
 
+  function listFlightDetailsCB(index) {
+    index = 0;
+    return <div>
+            <span>{props.flightData.data.slices[0].segments[0].operating_carrier.iata_code + " " + props.flightData.data.slices[0].segments[0].operating_carrier_flight_number}</span>
+
+            <td><span>{"Number of hops" + props.flightData.data.slices[0].segments.length}</span></td>
+            <span>{"Country of origin: " + props.flightData.data.slices[0].origin.iata_country_code}</span>
+            <td><span>{"Country of destination: " + props.flightData.data.slices[0].destination.iata_country_code}</span></td>
+            <span>{"Aircraft type: " + props.flightData.data.slices[0].segments[0].aircraft.name}</span>
+            <td><span>{"Fare basis code: " + props.flightData.data.slices[0].segments[0].passengers[0].fare_basis_code}</span></td>
+
+            <span>{"Aircraft type: " + props.flightData.data.slices[0].segments[0].aircraft.name}</span>
+            <span>{"Fare basis code: " + props.flightData.data.slices[0].segments[0].passengers[0].fare_basis_code}</span>
+          </div>
+  }
+
   return  <div >
-            <h1>Details about chosen flight {props.flightData.data.slices[0].segments[0].operating_carrier.iata_code + " " + props.flightData.data.slices[0].segments[0].operating_carrier_flight_number} :</h1> {/*props.flightData.slices[0].segments[0].operating_carrier.iata_code + props.flightData.slices[0].segments[0].operating_carrier_flight_number*/}
+            <h1>Details about chosen flights:</h1>
             
             <div className="flightResults">
               <div>
-                <span>{/*props.flightData*/}{/*JSON.stringify(props.flightData)*/}</span>
+                <p>{"Name of the company: " + props.flightData.data.owner.name}</p>
+                <p>{"Available services: " + props.flightData.data.available_services}</p>
+                
+                <td>{"Documents required to travel: " + valueTravel}</td>
+                <td><span>{"Passengers type: " + props.flightData.data.passengers.type}</span></td>
+
+                <td>{"Total taxes: " + props.flightData.data.tax_amount + " " + props.flightData.data.tax_currency}</td>
+                <td><span>{"Total kg of CO2 emissions: " + props.flightData.data.total_emissions_kg}</span></td>
               </div>
-              
               <div>
-                <td><span>{"Name of the company: " + props.flightData.data.owner.name}</span></td>
-                <span>{"Available services: " + props.flightData.data.available_services}</span>
-                <td><span>{"Documents required to travel: " + valueTravel}</span></td>
-                <span>{"Passengers type: " + props.flightData.data.passengers.type}</span>
-
-                <td><span>{"Total taxes: " + props.flightData.data.tax_amount + " " + props.flightData.data.tax_currency}</span>   </td>
-                <span>{"Total kg of CO2 emissions: " + props.flightData.data.total_emissions_kg}</span> 
-
-                <td><span>{"Number of hops" + props.flightData.data.slices[0].segments.length}</span></td>
-                <td><span>{"Country of origin: " + props.flightData.data.slices[0].origin.iata_country_code}</span></td>
-                <span>{"Country of destination: " + props.flightData.data.slices[0].destination.iata_country_code}</span>
-                <td><span>{"Aircraft type: " + props.flightData.data.slices[0].segments[0].aircraft.name}</span></td>
-                <span>{"Fare basis code: " + props.flightData.data.slices[0].segments[0].passengers[0].fare_basis_code}</span>
-
-                <td><span>{"Aircraft type: " + props.flightData.data.slices[0].segments[1].aircraft.name}</span></td>
-                <span>{"Fare basis code: " + props.flightData.data.slices[0].segments[1].passengers[0].fare_basis_code}</span>
-
+                {listFlightDetailsCB}
               </div>
             </div>
             
